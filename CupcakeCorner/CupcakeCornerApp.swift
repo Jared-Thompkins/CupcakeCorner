61d3fec8063158866b0584f3162d135bec677d7d
Jared-Thompkins <jaredthompkins21@gmail.com>
2023-02-02 02:12:41 -0600

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jared Thompkins on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
