98c59d27b96d0d1ff7b9b0c80b73a0fea9b08f20
Jared-Thompkins <jaredthompkins21@gmail.com>
2023-02-04 03:59:35 -0600

    add network request

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -13,34 +13,54 @@ struct CheckoutView: View {
     @State private var isLoading = true
     
     var body: some View {
-    
-        ScrollView {
-            VStack {
-                Image("Cupcake 1")
-                    .resizable()
-                    
-                    .onAppear() {
-                        self.isLoading = false
-                    }
-            }
-            .frame(height: 233)
-            if isLoading {
-                ProgressView()
-            }
-            
+        
+        VStack {
+            Spacer()
             Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                 .font(.title)
             
-            Button("Place Order", action: {})
-                .padding()
-    
+            Button("Place Order") {
+                Task {
+                    await placeOrder()
+                }
+                
+            }
+            .padding()
+            
+            Spacer()
+            Spacer()
+            
+            .navigationTitle("Check out")
+            .navigationBarTitleDisplayMode(.inline)
         }
-       
-        .navigationTitle("Check out")
-        .navigationBarTitleDisplayMode(.inline)
+        
+        
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+        } catch {
+            print("Checkout unsucessful.")
+        }
     }
 }
-
+    
+    
+    
+    
+    
+    
 struct CheckoutView_Previews: PreviewProvider {
     static var previews: some View {
         CheckoutView(order: Order())
